8b69a0353a7f4767b6d55302b714a3fd565d68f9
braglin <fabiobraglin@gmail.com>
2017-09-09 17:47:48 -0300

    Delete index.js

--- dist/js/index.js
+++
@@ -1,7 +0,0 @@
-$("#showToast").on('click', function (){
-				toastMe("Hi! I'm a toast!", 2000);
-});
-
-$("#showToastFA").on('click', function (){
-	toastMe('<i class="fa fa-cog fa-spin fa-3x fa-fw"></i>', 3000);
-});
