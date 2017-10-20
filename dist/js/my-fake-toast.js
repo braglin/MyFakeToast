a0fad848a0ebc889d958ba24f7a369385332a26b
braglin <fabiobraglin@gmail.com>
2017-10-20 06:37:02 -0200

    Add IOS style and fix FontAwesome usage

--- dist/js/my-fake-toast.js
+++ dist/js/my-fake-toast.js
@@ -1,9 +1,17 @@
 window.onload = function(){
 	var b = document.body.innerHTML;
-	document.body.innerHTML = b + "<div style='position: absolute;max-width: 98%; width: 98%; border: none; height: 0vh; top: 0px; left: 0px;'><div class='toastMe'>TESTE</div></div>";
+	document.body.innerHTML = b + "<div style='position: absolute;max-width: 100%; width: 100%; border: none; height: 0vh; top: 0px; left: 0px;'><div class='toastMe'>TESTE</div></div>";
+	var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+	var toastMeCss = ''
+	if (/android/i.test(userAgent)) {
+    	toastMeCss = '.toastMe { pointer-events:none; max-width: 95%;font-family: arial; font-size: 14px; overflow-wrap: break-word; position: relative;	margin: auto;	padding: 12px 14px;	z-index: 1000;	background: rgba(97,97,97, .95);	color: #fff;	text-shadow: 0px 0px 4px black;	text-align:center;	border-radius: 25px;	border: 1 solid rgba(50,50,50,1);	transition: display 1.5s; }';
+	}
+	if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    	toastMeCss = '.toastMe { pointer-events:none; max-width: 95%;font-family: arial; font-size: 14px; overflow-wrap: break-word; position: relative;	margin: auto;	padding: 12px 14px;	z-index: 1000; background-color: rgba(0, 0, 0, 0.8);	text-align:center; border-radius: 6px; color: #fff; transition: display 0.8s;}';
+	}
 	var style = document.createElement('style');
 	style.type = 'text/css';
-	style.innerHTML = '.toastMe { pointer-events:none; max-width: 90%;font-family: arial; font-size: 14px; overflow-wrap: break-word; position: relative;	margin: auto;	padding: 12px 14px;	z-index: 1000;	box-shadow: 1px 1px 25px black;	background: rgba(97,97,97, .95);	color: #fff;	text-shadow: 0px 0px 4px black;	text-align:center;	border-radius: 25px;	border: 1 solid rgba(50,50,50,1);	transition: display 1.5s; } .toastTop {top: 8vh} .toastCenter {top: 45vh;} .toastBot {top: 82vh;} .toastHide { display: none;} .animated {-webkit-animation-duration: 1s;  animation-duration: 1s;  -webkit-animation-fill-mode: both;  animation-fill-mode: both;}@-webkit-keyframes fadeIn {  0% {    opacity: 0;  }  100% {    opacity: 1;  }}@keyframes fadeIn {  0% {    opacity: 0;  }  100% {    opacity: 1;  }}.fadeIn {  -webkit-animation-name: fadeIn;  animation-name: fadeIn;}@-webkit-keyframes fadeOut {  0% {    opacity: 1;  }  100% {    opacity: 0;  }}@keyframes fadeOut {  0% {   opacity: 1;}  100% {    opacity: 0;  }}.fadeOut {  -webkit-animation-name: fadeOut;  animation-name: fadeOut;}';	
+	style.innerHTML = toastMeCss + '.toastTop {top: 8vh} .toastCenter {top: 45vh;} .toastBot {top: 82vh;} .toastHide { display: none;} .animated {-webkit-animation-duration: 1s;  animation-duration: 1s;  -webkit-animation-fill-mode: both;  animation-fill-mode: both;}@-webkit-keyframes fadeIn {  0% {    opacity: 0;  }  100% {    opacity: 1;  }}@keyframes fadeIn {  0% {    opacity: 0;  }  100% {    opacity: 1;  }}.fadeIn {  -webkit-animation-name: fadeIn;  animation-name: fadeIn;}@-webkit-keyframes fadeOut {  0% {    opacity: 1;  }  100% {    opacity: 0;  }}@keyframes fadeOut {  0% {   opacity: 1;}  100% {    opacity: 0;  }}.fadeOut {  -webkit-animation-name: fadeOut;  animation-name: fadeOut;}';	
 	document.getElementsByTagName('head')[0].appendChild(style);
 	var a = document.getElementsByClassName("toastMe");
 	a[0].classList.add("toastHide", "animated", "fadeOut");
@@ -12,11 +20,11 @@ window.onload = function(){
 function toastMe(msg, time, position) {
 	var a = document.getElementsByClassName("toastMe");
 	a[0].classList.remove("toastTop", "toastCenter", "toastBot");
+	a[0].innerHTML = msg;
+    a[0].style.width = (7*a[0].textContent.length) + 'px';
+    a[0].style.maxWidth = '90%';	
 	a[0].classList.add("toast"+position);
 	a[0].classList.remove("toastHide");
-	a[0].classList.replace("fadeOut", "fadeIn");
-    a[0].style.width = (7*msg.length) + 'px';
-    a[0].style.maxWidth = '90%';
-	a[0].innerHTML = msg;	
+	a[0].classList.replace("fadeOut", "fadeIn");	
 	setTimeout(function(){a[0].classList.replace("fadeIn", "fadeOut");}, time);
 }
--- example/js/index.js
+++ example/js/index.js
@@ -1,7 +1,16 @@
-$("#showToast").on('click', function (){
-				toastMe("Hi! I'm a toast!", 2000);
-});
+function toastBig() {
+  toastMe(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at gravida dolor. Maecenas et ipsum vitae massa dignissim eleifend eget ut leo.",
+    6000,
+    "Top"
+  );
+}
 
-$("#showToastFA").on('click', function (){
-	toastMe('<i class="fa fa-cog fa-spin fa-3x fa-fw"></i>', 3000);
-});
+function toastCenter() {
+  var spin = '<i class="fa fa-spinner fa-pulse" aria-hidden="true" style="font-size: 30px;margin-bottom: 10px;"></i>';
+  toastMe(spin + "<br> Loading...", 6000, "Center");
+}
+
+function toastBot() {
+  toastMe("Toast me at the bottom!", 6000, "Bot");
+}
